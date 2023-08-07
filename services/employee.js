d55df00585a1549d87500a66e25d394d194b6f5b
Roy Tarantino <roy.tarantino@verizonwireless.com>
2023-08-07 19:29:56 -0400

    - Added comments to service layer

--- services/employee.js
+++ services/employee.js
@@ -2,16 +2,19 @@ const {storeData, loadData} = require("../util/objstore");
 let employees = [];
 const service = {};
 
+// Add a new employee
 service.postEmployee = async (req, res) => {
     employees.push(JSON.parse(JSON.stringify(req.query)));
     console.log('post emp');
 
 }
 
+// Return all employees
 service.getAllEmployees = async (req, res) => {
     return employees
 }
 
+// Return a single employee
 service.getEmployee = async (req, res) => {
     return employees.filter(emp =>
         emp.number === req.query.number ||
@@ -20,6 +23,7 @@ service.getEmployee = async (req, res) => {
 
 }
 
+// Delete an employee
 service.deleteEmployee = async (req, res) => {
     const delIndex = employees.findIndex(emp =>
         emp.number === req.query.number ||
@@ -29,10 +33,14 @@ service.deleteEmployee = async (req, res) => {
 }
 
 
+// Load employees from a file: query parameter employeefile
+//  default: my_employees
 service.loadEmployees = async (req, res) => {
     employees = JSON.parse(JSON.parse(loadData(req.query.employeeFile?req.query.employeeFile:'my_employees')));
 }
 
+// Save employees toa file: query parameter employeefile
+//  default: my_employees
 service.saveEmployees = async (req, res) => {
     storeData(JSON.stringify(employees), req.query.employeeFile?req.query.employeeFile:'my_employees');
 }
